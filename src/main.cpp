6586c1dae32ca5ec7b0f0aef5bac4ac7bd126fec
Andrea Pollini <prof.andrea.pollini@gmail.com>
2024-12-17 18:37:17 +0100

    project first import

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,78 @@
+#include <vector>
 
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define CELL_SIZE 5
+#define GRID_WIDTH (SCREEN_WIDTH / CELL_SIZE)
+#define GRID_HEIGHT (SCREEN_HEIGHT / CELL_SIZE)
+
+void DrawWorld(const std::vector<bool> &world) {
+    for (int y = 0; y < GRID_HEIGHT; y++) {
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            if (world[y * GRID_WIDTH + x]) {
+                DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, LIGHTGRAY);
+            }
+        }
+    }
+}
+void UpdateWorld(std::vector<bool>& world)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    for (int y = GRID_HEIGHT - 1; y >= 0; y--)
+    {
+        for (int x = 0; x < GRID_WIDTH; x++)
+        {
+            if (world[y * GRID_WIDTH + x])
+            {
+                if (y < GRID_HEIGHT - 1)
+                {
+                    if (!world[(y + 1) * GRID_WIDTH + x])
+                    {
+                        world[y * GRID_WIDTH + x] = false;
+                        world[(y + 1) * GRID_WIDTH + x] = true;
+                    }
+                    else
+                    {
+                        int dx = GetRandomValue(-1, 1);
+                        if (x + dx >= 0 && x + dx < GRID_WIDTH && !world[(y + 1) * GRID_WIDTH + x + dx])
+                        {
+                            world[y * GRID_WIDTH + x] = false;
+                            world[(y + 1) * GRID_WIDTH + x + dx] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+void AddSand(std::vector<bool> &world, int mouseX, int mouseY) {
+    int x = mouseX / CELL_SIZE;
+    int y = mouseY / CELL_SIZE;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    if (x >= 0 && x < GRID_WIDTH && y >= 0 && y < GRID_HEIGHT) {
+        world[y * GRID_WIDTH + x] = true;
+    }
+}
+
+int main() {
+    std::vector<bool> world(GRID_WIDTH * GRID_HEIGHT, false);
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Falling Sand Simulation");
+
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            AddSand(world, GetMouseX(), GetMouseY());
+        }
+        UpdateWorld(world);
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -30,7 +80,7 @@ int main(void)
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawWorld(world);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -38,7 +88,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
